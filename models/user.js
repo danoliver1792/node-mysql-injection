d1d7a5fa763a7e60a5065366f16fbb3fb6679593
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-10-05 23:53:43 -0300

    conexao com o mysql

---
+++ models/user.js
@@ -0,0 +1,18 @@
+// modelo de conexao com o banco de dados
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+});
+
+module.exports = User;
